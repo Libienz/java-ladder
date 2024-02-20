7391c4ee65ceb96ea4e9c18a9cf17513408bfbe7
libienz <leekh10195@gmail.com>
2024-02-20 15:34:34 +0900

    test: [TDD TEST FAILS] Line 클래스 테스트 작성

---
+++ src/test/java/domain/LineTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("사다리 가로 줄 테스트")
+public class LineTest {
+
+    @DisplayName("가로 줄이 연속되는 경우 줄을 생성할 수 없다")
+    @Test
+    void testCreateLineWithContinuousValue() {
+        List<Boolean> points = List.of(true, true, false);
+        assertThatThrownBy(() -> new Line(points))
+                .isInstanceOf()
+                .hasMessage("");
+    }
+
+    @DisplayName("가로 줄이 연속되는 경우가 없으면 줄을 생성할 수 있다")
+    void testCreateLineWithNoContinuousValue() {
+        List<Boolean> points = List.of(true, false, true);
+        assertThatCode(() -> new Line(points)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("특정 좌표에 가로 줄이 있는 지 확인할 수 있다")
+    @Test
+    void testCheckConnection() {
+        List<Boolean> points = List.of(true, false, true);
+        Line line = new Line(points);
+        assertThat(line.hasConnection(0)).isTrue();
+    }
+
+    @DisplayName("특정 좌표에 가로 줄이 없는 지 확인할 수 있다")
+    @Test
+    void testCheckNoConnection() {
+        List<Boolean> points = List.of(true, false, true);
+        Line line = new Line(points);
+        assertThat(line.hasConnection(1)).isFalse();
+    }
+}
