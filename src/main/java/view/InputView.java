7944afa12d9c3e3ef0a50060dc6ca437da981e15
libienz <leekh10195@gmail.com>
2024-02-20 17:09:16 +0900

    feat: InputView 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String requestNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
+        return scanner.nextLine();
+    }
+
+    public String requestHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return scanner.nextLine();
+    }
+}
