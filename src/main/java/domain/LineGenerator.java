06d71403516e754cc536f0deaa84f38ad1dc5a4c
libienz <leekh10195@gmail.com>
2024-02-20 18:04:06 +0900

    fix: LineGenerator 연결 상태 리스트 관리 수정

--- src/main/java/domain/LineGenerator.java
+++ src/main/java/domain/LineGenerator.java
@@ -8,13 +8,13 @@ public class LineGenerator {
     public Line generateLine(int personCount) {
         List<ConnectStatus> points = new ArrayList<>();
 
-        for (int i = 0; i < personCount; i++) {
+        for (int i = 0; i < personCount - 1; i++) {
             ConnectStatus randomStatus = ConnectStatus.randomStatus();
             if (i != 0 && randomStatus == ConnectStatus.CONNECTED && points.get(i - 1) == ConnectStatus.CONNECTED) {
                 points.add(ConnectStatus.UNCONNECTED);
                 continue;
             }
-            points.add(ConnectStatus.CONNECTED);
+            points.add(randomStatus);
         }
         return new Line(points);
     }
