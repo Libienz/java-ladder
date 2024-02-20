09a5f9b4300ce44649b5ec4c6a311e70211ae1a8
libienz <leekh10195@gmail.com>
2024-02-20 16:29:16 +0900

    feat: ConnectStatus enum 구현

---
+++ src/main/java/domain/ConnectStatus.java
@@ -0,0 +1,19 @@
+package domain;
+
+public enum ConnectStatus {
+    CONNECTED(Boolean.TRUE),
+    UNCONNECTED(Boolean.FALSE);
+
+    private Boolean status;
+
+    ConnectStatus(Boolean status) {
+        this.status = status;
+    }
+
+    private ConnectStatus randomStatus() {
+        int min = 0;
+        int max = values().length;
+        int idx = (int) (Math.floor(Math.random() * (max - min)) + min);
+        return values()[idx];
+    }
+}
