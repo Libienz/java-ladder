62dcdedc1360416c9ca48d78f2e22500c8f3c4ce
libienz <leekh10195@gmail.com>
2024-02-20 17:18:19 +0900

    feat: 이름의 일급 컬렉션 Names 구현

---
+++ src/main/java/domain/Names.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class Names {
+    private final List<Name> names;
+
+    public Names(List<Name> names) {
+        this.names = names;
+    }
+}
